dbc14f86a2cf8b4e3d1ca5d9fdbd19c0e4803543
nathanramli <hezkyanatanael84@gmail.com>
2021-06-18 02:49:58 +0700

    add new fields for pasien model

--- src/main/java/com/crud/covid/controller/PasienController.java
+++ src/main/java/com/crud/covid/controller/PasienController.java
@@ -1,6 +1,7 @@
 package com.crud.covid.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/crud/covid/model/Pasien.java
+++ src/main/java/com/crud/covid/model/Pasien.java
@@ -1,11 +1,15 @@
 package com.crud.covid.model;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Table(name = "pasien")
@@ -19,6 +23,13 @@ public class Pasien {
     public String nik;
     @Column(name = "nama")
     public String nama;
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    @Column(name = "tanggal_lahir")
+    public Date tanggalLahir;
+    @Column(name = "jenis_kelamin")
+    public String jenis_kelamin;
+    @Column(name = "alamat")
+    public String alamat;
 
     public void setId(long id) {
         this.id = id;
@@ -32,6 +43,18 @@ public class Pasien {
         this.nama = nama;
     }
 
+    public void setTanggalLahir(Date tanggalLahir) {
+        this.tanggalLahir = tanggalLahir;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public void setJenisKelamin(String jenis_kelamin) {
+        this.jenis_kelamin = jenis_kelamin;
+    }
+
     public long getId() {
         return this.id;
     }
@@ -42,5 +65,17 @@ public class Pasien {
 
     public String getNama() {
         return this.nama;
+    }
+
+    public Date getTanggalLahir() {
+        return this.tanggalLahir;
+    }
+
+    public String getAlamat() {
+        return this.alamat;
+    }
+
+    public String getJenisKelamin() {
+        return this.jenis_kelamin;
     }
 }
